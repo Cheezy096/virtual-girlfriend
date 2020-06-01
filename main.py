8229d85733a45f87df2e7fd4e159c8b543f9ef6d
Cheezy <33290752+Cheezy096@users.noreply.github.com>
2020-06-01 23:59:19 +0100

    meme theory

--- main.py
+++ main.py
@@ -2,41 +2,49 @@
 
 import json
 
-conf = json.load(open("./config.json"))
-
+config = json.load(open("./config.json"))
 relationship = True 
-commands = ["sex", "hash", "cum", "linux"] 
 age = None
 
 # Age 0: Underage
 # Age 1: Adult
-age = 0 if int(conf["age"]) < 19 else 1 
+age = 0 if int(config["age"]) < 18 else 1 
 
 def log(logType=None, Text=None):
-    if logType == "error": print(f"\033[31m[Error]\033[0m {Text}")
-    if logType == "success": print(f"\033[92m[Success]\033[0m {Text}")
-    if logType == "warn": print(f"\033[93m[Warning]\033[0m {Text}")
-    if logType == "info": print(f"\033[96m[Info]\033[0m {Text}")
-    if logType == "notice": print(f"\033[95m[Notice]\033[0m {Text}")
+    if logType == "error": print(f"[Error] {Text}")
+    if logType == "success": print(f"[Success] {Text}")
+    if logType == "warn": print(f"[Warning] {Text}")
+    if logType == "info": print(f"[Info] {Text}")
+    if logType == "notice": print(f"[Notice] {Text}")
+
+commands = {
+    0 : {
+        "sex" : f"{config['gfname']} >> What the heck you super duper creepy perv!",
+        "hash" : f"{config['gfname']} >> He's a loser.",
+        "cum" : f"{config['gfname']} >> What the heck you super duper creepy perv!",
+        "linux" : f"{config['gfname']} >> Shut up."
+    },
+    1 : {
+        "sex" : f"{config['gfname']} >> Fuck off {config['bfname']}.",
+        "hash" : f"{config['gfname']} >> Will be getting a restraining order.",
+        "cum" : f"{config['gfname']} >> Go kill yourself.",
+        "linux" : f"{config['gfname']} >> No one likes you {config['bfname']}."
+    },
+    "default" : f"{config['gfname']} >> Go away."
+}
 
 log("info","To enter SETUP press F12 or DEL")
 log("info","Starting virtual girlfriend...")
-log("info",f"Her name: {conf['gfname']}")
-log("info",f"Her age: {conf['age']}")
-log("info",age)
+log("info",f"Her name: {config['gfname']}")
+log("info",f"Her age: {config['age']}")
+log("info",f"Your name: {config['bfname']}")
 print("\n")
 
 while relationship: 
     inp = input(f">>> ")
-    if age == 0: 
-        if commands[0] in inp.lower() or commands[2] in inp.lower(): print(f"{conf['gfname']} >> What the heck you super duper creepy perv!"); quit() 
-        if commands[1] in inp.lower(): print(f"{conf['gfname']} >> He's a loser.")
-        if commands[3] in inp.lower(): print(f"{conf['gfname']} >> Shut up.")
-        if inp.lower() not in commands: print(f"{conf['gfname']} >> Go away.")
-    elif age == 1: 
-        if commands[0] in inp.lower(): print(f"{conf['gfname']} >> Fuck off {conf['bfname']}.")
-        if commands[1] in inp.lower(): print(f"{conf['gfname']} >> Will be getting a restraining order.")
-        if commands[2] in inp.lower(): quit()
-        if commands[3] in inp.lower(): print(f"{conf['gfname']} >> No one likes you {conf['bfname']}.")
-        if inp.lower() not in commands: print(f"{conf['gfname']} >> Leave me alone.")
-    else: log("error","Age is not 0 or 1."); quit()
+    if inp.lower() in ["sex", "cum"]: print(commands[age]["sex"])
+    elif inp.lower() == "hash": print(commands[age]["hash"])
+    elif inp.lower() == "linux": print(commands[age]["linux"])
+    elif inp.lower() == "cum": print(commands[age]["cum"])
+    elif inp.lower() not in commands: print(commands["default"])
+
